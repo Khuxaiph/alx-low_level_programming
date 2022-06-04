7680a7dbc3b15c59a00c584866da5a0dbc7fc8c3
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-04 16:43:17 -0700

    zrr

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,14 +8,20 @@
  */
 int main(void)
 {
-int n;
-char ch;
+int n, last;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-for (ch = 'a'; ch <= 'z'; ch++)
+last = n % 10;
+if (n > 5)
 {
-putchar(ch);
+printf("Last digit of %d is %d and is greater than 5", n, last);
 }
-putchar('\n');
+else if (n == 0)
+{
+printf("Last digit of %d is %d and is 0", n, last);
+}
+else if (n < 6; n != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
 return (0);
 }
