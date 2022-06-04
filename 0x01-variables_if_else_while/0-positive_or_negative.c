f0cb4792ea21ac0aee0028010cea51ac51ad3a5a
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-04 14:39:37 -0700

    file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,9 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
+int n; 
+ srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
 if (n > 0)
 printf("%d is possitive\n", n);
 else if (n == 0)
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n;
+  char ch;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	  {
+	    putchar(ch);
+	  }
+	putchar('\n');
+	return (0);
+}
