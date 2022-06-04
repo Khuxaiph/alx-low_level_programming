2de45f4ff06e4c28d4d38f38543db48db7d615f5
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-04 14:42:59 -0700

    luck

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,16 +6,15 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
-  char ch;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	for (ch = 'a'; ch <= 'z'; ch++)
-	  {
-	    putchar(ch);
-	  }
-	putchar('\n');
-	return (0);
+int n;
+char ch;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
