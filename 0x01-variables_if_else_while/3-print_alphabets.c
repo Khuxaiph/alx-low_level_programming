e2fd6b3ceb238bf5b2e96d0940c200d17019774f
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-04 17:13:42 -0700

    file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+char ch;
+char up;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (up = 'A'; up <= 'Z'; up++)
+{
+putchar(up);
+}
+putchar('\n');
+return (0);
+}
