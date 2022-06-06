4ad99c0ba081e98fc5c62f1e4f532bf2960e250a
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-06 14:58:59 -0700

    gg

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
- */
+/*
+*
+* main - assigns a random number to int n everytime
+* it executes, and prints it
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int n, last;
+int n;
+int last;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
-if (n > 5)
-{
-printf("Last digit of %d is %d and is greater than 5", n, last);
-}
-else if (n == 0)
-{
-printf("Last digit of %d is %d and is 0", n, last);
-}
-else if (n < 6; n != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+if (last > 0)
+printf("Last digit of %i is %i and is greater than 5\n", n, last);
+else if (last == 0)
+printf("Last digit of %i is %i and is 0\n", n, last);
+else if (last < 0)
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 return (0);
 }
