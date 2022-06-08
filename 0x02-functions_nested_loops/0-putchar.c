0852605d2a49bb4994a7903a9420dbdf58754605
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-07 18:27:55 -0700

    erf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-
-for (g = 'a'; g <= 'z'; g++)
+/**
+* num - printing a to z,
+* with increment
+* hopefully it works
+* Return: Always 0 (sucess)
+*/
+int num(void)
 {
-putchar(g);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
 }
 putchar('\n');
+return (0);
+}
