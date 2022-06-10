09272d8a72aac96adcb99ebec5e66ef0c88b79f8
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-10 10:49:26 -0700

    zzuz

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,21 +1,24 @@
-#include <stdlib.h>
-#include <time.h>#include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*
+	*while (i < 10)
+	*{
+	*Infinite loop - No increase of variable
+	*putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
