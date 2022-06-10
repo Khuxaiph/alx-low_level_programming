dfc2534ac5cef03a5e53350fbfbaa970155f7c2b
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-10 13:47:22 -0700

    rrram

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,32 +7,15 @@
  */
 void more_numbers(void)
 {
-	int w;
-	int x;
-	int y;
-	int z;
-
-	w = 0;
-	x = 0;
-	while (w < 10)
-	{
-		while (x <= 14)
-		{
-			if (x < 10)
-			{
-				z = x;
-			}
-			else
-			{
-				y = x / 10;
-				z = x % 10;
-				_putchar (y + '0');
-			}
-			_putchar(z + '0');
-			x++;
-		}
-		w++;
-		x = 0;
-		_putchar('\n');
-	}
+  int num;
+  int count;
+  if (num = 0; num <= 10; num++)
+    {
+    while (count < 10 )
+      {
+	_putchar();
+	num++;
+      }
+    _putchar('num');
+    }
 }
