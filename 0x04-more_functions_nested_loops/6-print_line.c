f37326ef48f8290cf4a345f017c3eb69f2df4066
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-10 15:43:31 -0700

    g

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line: to print a straight line using the syntax putchar
+ * n: hold the line
+ * Return: return 0 if successful
+ * void print_line(int n): it is one ofthe xters i am to use
+*/
+void print_line(int n)
+{
+int n;
+for (n = 0; n < 15; n++)
+{
+_putchar('_');
+}
+return (0);
+}
