0b13dbec60d1cb8750ba84e8f901e87665c12d4e
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-10 10:37:05 -0700

    OG

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
+void positive_or_negative(int);
+void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
