09c973b4fde88c213c24d43cbc59ec954df9d74a
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-13 16:12:00 -0700

    file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - switch the values of two numbers
+ * @a : first digit
+ * @b : second digit
+ * return : if 0, sucess
+ */
+void swap_int(int *a, int *b)
+{
+int c = 10;
+int d = 20;
+swap(&c, &d);
+return (0);
+}
