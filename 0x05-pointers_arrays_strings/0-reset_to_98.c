bec9b9c66dd3ed02182bd4b573e29ba5ab51f42b
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-13 08:07:49 -0700

    file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98.c: Rest value to 98
+ * @n : First value
+ * Return : if it returns 0, sucess
+ */
+
+void reset_to_98(int *n)
+{
+  int a = 10;
+  
+}
