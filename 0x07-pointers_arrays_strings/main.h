634bede43c30767de8966ac2c68587129d9a57ab
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-21 17:08:56 -0700

    abeg

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void print_chessboard(char (*a)[8]);
