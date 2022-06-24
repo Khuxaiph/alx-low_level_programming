3287c2a62975aa4b283d93540e05c7d4a9a4d3e1
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-06-23 17:35:09 -0700

    haka

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,19 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: the string
- * Return: return zero
  *
+ * @s: The string that prints
+ *
+ * Return: nothing
  */
 
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-return;
-}
-else
-{
-_puts_recursion(s);
-_putchar(*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
