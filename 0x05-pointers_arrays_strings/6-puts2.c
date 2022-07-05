58a1443ff628cfb20203ef6aca281d0a4f4b086f
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-07-05 10:35:56 -0700

    gh

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,19 +9,16 @@
 
 void puts2(char *str)
 {
-  int i;
-  int len;
-
-  i = 0;
-  len = _strlen(str) - 1;
-
-  while (i <= len)
-    {
-      _putchar(str[i]);
-      i += 2;
-    }
-
-  _putchar('\n');
+int i;
+int len;
+i = 0;
+len = _strlen(str) - 1;
+while (i <= len)
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
 }
 #include "main.h"
 /**
@@ -34,10 +31,8 @@ void puts2(char *str)
 
 int _strlen(char *s)
 {
-  int i = 0;
-
-  while (s[i] != '\0')
-    i++;
-
-  return (i);
+int i = 0;
+while (s[i] != '\0')
+i++;
+return (i);
 }
