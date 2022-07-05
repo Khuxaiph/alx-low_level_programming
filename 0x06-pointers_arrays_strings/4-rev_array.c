1fe998575a2d372b5a76268b18dc5b0d3c5cf593
Huzaifa Ahmed <khuxaiphiahmad@gmail.com>
2022-07-05 11:20:41 -0700

    file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,17 +10,14 @@
 
 void reverse_array(int *a, int n)
 {
-  int temp; /* holds value */
-  int i;    /* counter */
-  int j = n - 1; /* reverse counter */
-
-  for (i = 0; i < j; i++)
-    {
-      temp = a[i];
-      a[i] = a[j];
-      a[j] = temp;
-      j--;
-    }
-
-
+int temp; /* holds value */
+int i;    /* counter */
+int j = n - 1; /* reverse counter */
+for (i = 0; i < j; i++)
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+j--;
+}
 }
